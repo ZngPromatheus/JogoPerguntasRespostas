1abcfe7f34469433d3dc3046c4a9e790ea9cbb16
ZngPromatheus <matheus.santana.zen@gmail.com>
2017-02-01 17:28:13 -0200

    Arrumamos mais umas paradas na nossa tabela.

--- Docs/Jogo_Perguntas_e_Respostas.sql
+++ Docs/Jogo_Perguntas_e_Respostas.sql
@@ -16,6 +16,7 @@ create table tb_Perguntas(
     id int identity primary key,
     pergunta varchar(max),
     resposta_correta varchar(max),
+    id_jogador int,
     data    datetime default getdate()
     )
     
@@ -33,3 +34,4 @@ insert into tb_Jogador(nome) values('Ramon')
 --Limpa os dados da tabela e reinicia o campo identily
 truncate table tb_Jogador
 
+alter table tb_jogador add id_jogador int
